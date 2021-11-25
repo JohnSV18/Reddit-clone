5534748b598c05efb2805078ca817582f2225c20
John Marcos <johnsaguay@gmail.com>
2021-11-24 16:21:06 -0800

    Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
-const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 module.exports = (app) => {
     app.get('/sign-up', (req, res) => {
         res.render('sign-up');
@@ -7,17 +8,51 @@ module.exports = (app) => {
 
     app.post('/sign-up', (req, res) => {
         const user = new User(req.body);
-
         user
-            .save()
-            .then((user) => {
-                const token = jwt.sign({ _id: user._id }, proccess.env.SECRET, { expiresIn: '60 days' });
-                res.cookie('nToken', token, { maxAge: 900000, httpOnly:true });
-                return res.redirect('/')
-            })
+          .save()
+          .then((user) => {
+              const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
+              res.cookie('nToken', token, { maxAge: 900000, httpOnly:true });
+              return res.redirect('/')
+        })
+          .catch((err) => {
+              console.log(err.message);
+              return res.status(400).send({ err });
+          })
+    });
+    app.get('/login', (req, res) => res.render('login'));
+
+    app.post('/login', (req, res) => {
+        const { username, password } = req.body;
+        //find this username
+        User.findOne({ username }, 'username password')
+          .then((user) => {
+              if (!user) {
+                  //User not found
+                  return res.status(401).send({ message: "Wrong Username or Password" });
+              }
+              //check the password
+              user.comparePassword(password, (err, isMatch) => {
+                  if (!isMatch) {
+                      //passwrord does not match
+                      return res.status(401).send({ message: 'Wrong Username or password' });
+                  }
+                  //create a token
+              const token = jwt.sign({ _id: user._id , username: user.username }, process.env.SECRET, {
+                  expiresIn: '60 days',
+              });
+              //Set a cookie and redirect to root
+              res.cookie('nToken', token, { maxAge: 900000, httpOnly: true })
+              return res.redirect('/')
+              })
+          })
             .catch((err) => {
-                console.log(err.message);
-                return res.status(400).send({ err });
-            })
-    })
+                console.log(err);
+        });
+    });
+
+    app.get('/logout', (req, res) => {
+        res.clearCookie('nToken');
+        return res.redirect('/');
+    });
 }
