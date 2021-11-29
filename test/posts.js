8deff0d1f3238c03211a584590af9492a3ad9712
John Marcos <johnsaguay@gmail.com>
2021-11-29 00:07:31 -0800

    Fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -1,12 +1,14 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const { describe, it, after } =  require('mocha');
+const { describe, it, before, after } =  require('mocha');
 
 // Import the Post model from our models folder so we can use it in our tests
 const Post = require("../models/post");
+const User = require('../models/user');
 const app = require("../server");
 
 const should = chai.should();
+const agent = chai.request.agent(app);
 
 chai.use(chaiHttp);
 
@@ -14,13 +16,30 @@ describe('Posts', function () {
     const newPost = {
         title: 'post title',
         url: 'https://www.google.com',
-        summary: 'post summary'
+        summary: 'post summary',
+        
     };
+    const user = {
+      username: 'poststest',
+      password: 'testposts',
+    }
+    before(function (done) {
+      agent
+        .post('/sign-up')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(user)
+        .then(function (res) {
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        })
+    })
     it('should create with valid attributes at POST /posts/new', function (done) {
         // TODO: test code goes here!
         Post.estimatedDocumentCount()
         .then(function (initialDocCount) {
-          chai.request(app)
+          agent
             .post('/posts/new')
             // This line fakes a form post,
             // since we're not actually filling out a form
@@ -33,7 +52,7 @@ describe('Posts', function () {
                   // Check that the database has status 200
                   res.should.have.status(200);
                   // Check that the database has one more post in it
-                  newDocCount.should.equal(initialDocCount + 1)
+                  newDocCount.should.equal(initialDocCount + 1);
                   done();
                 })
                 .catch(function (err) {
@@ -47,8 +66,26 @@ describe('Posts', function () {
         .catch(function (err) {
             done(err);
     });
-    after(function () {
-        Post.findOneAndDelete(newPost);
-    });
   })
+    after(function (done) {
+        Post.findOneAndDelete(newPost)
+        .then(function () {
+          agent.close();
+
+          User
+            .findOneAndDelete({
+              username: user.username,
+            })
+            .then(function () {
+              done();
+            })
+            .catch(function (err) {
+              done(err);
+            });
+        })
+        .catch(function (err) {
+          done(err);
+        })
+    });
+  
 })
