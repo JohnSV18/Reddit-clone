ae692a07bfc2af5850d5f17014949cc5b97fc08c
John Marcos <johnsaguay@gmail.com>
2021-11-28 23:06:22 -0800

    Implemented initial authentication tests

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -4,6 +4,7 @@ const checkAuth = (req, res, next) => {
     console.log('Checking Authentication');
     if (typeof req.cookies.nToken === 'undefined' || req.cookies.nToken === null) {
         req.user = null;
+        console.log('No token found');
     } else {
         const token = req.cookies.nToken;
         const decodedToken = jwt.decode(token, { complete: true}) || {};
